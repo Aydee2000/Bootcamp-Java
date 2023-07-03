228c2bf81e1b05c7e42d6adb7ca625ff6c1ad2dc
Aydee2000 <aydeeysabeltoledo@gmailcom>
2023-07-03 17:25:53 -0300

    Agregando ejercicio 1 de C10

--- Programacion_Orientada_a_Objetos_Clase_10/Persona.java
+++ Programacion_Orientada_a_Objetos_Clase_10/Persona.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class Persona {
     public static void main(String[] args) {
+       /* Vamos a crear una clase llamada Persona. Sus atributos son: id, nombre, edad, fecha
+        de nacimiento y DNI. Construye los siguientes métodos para la clase:
+
+        ● Un constructor, donde los datos pueden estar vacíos.
+        ● Los setters y getters para cada uno de los atributos.
+        ● mostrar(): Muestra los datos de la persona.
+        ● esMayorDeEdad(): Devuelve un valor lógico indicando si es mayor de edad
+        */
 
         DatosDePersona newperson= cargarDatos();
         newperson.mostrarDatos();
